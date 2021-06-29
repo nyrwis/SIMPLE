af4147ec81b7cbc10e636f6201e9c02375b4a01c
nyrwis <meqnyrwis@gmail.com>
2021-06-29 15:31:31 +0900

    change sudo code to python code
    Add: code for blokus.py
    Add: Pos(), Point() in classes.py

--- app/environments/blokus/blokus/envs/blokus.py
+++ app/environments/blokus/blokus/envs/blokus.py
@@ -1,3 +1,5 @@
+# Modified code from https://github.com/DerekGloudemans/Blokus-Reinforcement-Learning
+
 import gym
 import numpy as np
 
@@ -15,12 +17,15 @@ class BlokusEnv(gym.Env):
         self.name = 'blokus'
         self.manual = manual
 
+        self.board_length = 8
+        self.n_player = 2
+        self.board_shape = (self.board_length, self.board_length)
+        self.piece_num = 8
+        self.possible_action_num = 1269
+        self.action_space = gym.spaces.Discrete(self.possible_action_num)
+        self.observation_space = gym.spaces.Box(low=0, high=self.n_player, self.board_shape)
         self.verbose = verbose
 
-    @property
-    def legal_actions(self):
-        #calculate legal_actions based on board state, player's pieces
-        pass
 
     @property
     def observation(self):
@@ -28,13 +33,43 @@ class BlokusEnv(gym.Env):
         #return combined observation
         pass
     
+
+    @property
+    def legal_actions(self, action):
+        #calculate legal_actions based on board state, player's pieces
+        legal_action = 
+        for piece in self.current_player:
+            for point in get_point(self.current_player.possible_point):
+                for loc in self.current_player.loc:
+                    
+                    
+
+
+    @property
+    def current_player(self):
+        return self.players[self.current_player_num]
+
+
+    def get_point(self, board):
+        point_list = []
+        for col in board:
+            for row in col:
+                point = Point(col, row)
+                point_list.append(point)
+        return point_list
+
+
     def reset(self):
-        #set board
-        #set player
-            #hand out pieces
-            #set initial starting point
-        #return observation
-        pass
+        self.board = [[0 for i in range(self.board_length)] for j in range(self.board_length)]
+        self.players = [Player(1), Player(2)]
+        for player in self.players:
+            player.setup(self.board_length, self.piece_num)
+        self.current_player_num = 0
+        self.turns_taken = 0
+        self.done = False
+        logger.debug(f'\n\n---- NEW GAME ----')
+        return self.observation
+
 
     def step(self, action):
         #update board
@@ -52,8 +87,24 @@ class BlokusEnv(gym.Env):
         #return obseration, reward, done
         pass
 
+
     def render(self, mode='human', close=False, verbose = True):
         #print current game state
         #print current board state
         #print legal_actions
-        pass
+        logger.debug('')
+        if close:
+            return
+        if self.done:
+            logger.debug(f'GAME OVER')
+        else:
+            logger.debug(f"It is Player {self.current_player.id}'s turn to move")
+
+        for line in self.board:
+            logger.debug(' '.join(line))
+
+        if self.verbose:
+            pass
+
+        if not self.done:
+            logger.debug(f'\nLegal actions: {[i for i,o in enumerate(self.legal_actions) if o != 0]}')
--- app/environments/blokus/blokus/envs/classes.py
+++ app/environments/blokus/blokus/envs/classes.py
@@ -1,16 +1,53 @@
 class Piece():
-    def __init__(self, id, n, pos, pos):
+    def __init__(self, id, n, dim, pos):
         self.id = id
         self.n = n
+        self.dim = dim
         self.pos = pos
         self.pos_list = []
+
+
+class Pos():
+    def __init__(self, h, w, loc):
+        self.h = h
+        self.w = w
+        #self.loc = [[0 for i in range(h)] for j in range(w)]
+        #tuple for point of piece as list
+        self.loc = 
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 class Player():
     def __init__(self, id):
         self.id = id
-        #player's piece information
-        self.pieces = None
-        self.occupied_grid = None
-        self.corners_grid = None
-        self.adjacents_grid = None
-        self.possible_grid = None
+
+
+    def setup(self, board_length, piece_num):
+        self.pieces = [0 for i in range(piece_num)]
+        self.occupied = [[0 for i in range(board_length)] for j in range(board_length)]
+        self.corners = [[0 for i in range(board_length)] for j in range(board_length)]
+        self.adjacents = [[0 for i in range(board_length)] for j in range(board_length)]
+        self.possible = [[0 for i in range(board_length)] for j in range(board_length)]
+        
+        self.possible[0][0] = self.possible[0][-1] = self.possible[-1][0] = self.possible[-1][-1] = 1
+
+
+    def update(self, action):
+        occupied_update(action)
+        corners_update()
+        adjacents_update()
+        possible_update()
+
+
+    def occupied_update(self):
+        
+    def corners_update(self):
+
+    def adjacents_update(self):
+
+    def possible_update(self):
