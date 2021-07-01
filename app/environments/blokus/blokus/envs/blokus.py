70c4e0fb4af78764c421fa8035f6067a55f12595
nyrwis <meqnyrwis@gmail.com>
2021-07-01 10:29:51 +0900

    add code for legal_action, step

--- app/environments/blokus/blokus/envs/blokus.py
+++ app/environments/blokus/blokus/envs/blokus.py
@@ -11,18 +11,23 @@ from stable_baselines import logger
 from classes import Piece, Player
 
 
-p1 = [1, 1, (1,1), 1, [[1, 1, [(0,0)]]]]
-p2 = [2, 2, (2,1), 2, [[2, 1, [(0,0), (0,1)]], [1, 2, [(0,0), (1,0)]]]]
-p3 = [3, 3, (2,2), 4, [[2, 2, [(0,0), (0,1), (1,1)]], [2, 2, [(0,1), (1,1), (1,0)]], [2, 2, [(0.0), (1,0), (1,1)]], [2, 2, [(1,0), (0,0), (0,1)]]]]
-p4 = [4, 3, (3,1), 2, [[3, 1, [(0,0), (1,0), (2,0)]], [1, 3, [(0,0), (0,1), (0,2)]]]]
-p5 = [5, 4, (2,2), 1, [[2, 2, [(0,0), (0,1), (1,1), (1,0)]]]]
-p6 = [6, 4, (3,2), 4, [[3, 2, [(0,1), (1,0), (1,1), (1,2)]], [2, 3, [(0,0), (1,0), (1,1), (2,0)]], [3, 2, [(0,0), (0,1), (0,2), (1,1)]], [2, 3, [(0,1), (1,0), (1,1), (0,2)]]]
-p7 = [7, 4, (4,1), 2, [[4, 1, [(0,0), (0,1), (0,2), (0,3)]], [1, 4, [(0,0), (1,0), (2,0), (3,0)]]]]
-p8 = [8, 4, (3,2), 8, [[3, 2, [(0,2), (1,0), (1,1), (1,2)]], [2, 3, [(0,0), (1,0), (2,0), (2,1)]], [3, 2, [(0,0), (0,1), (0,2), (1,0)]], [2, 3, [(0,0), (0,1), (1,1), (2,1)]], [2, 3, [(0,0), (1,0), (1,1), (1,2)]], [3, 2, [(0,0), (0,1), (1,0), (2,0)]], [2, 3, [(0,0), (0,1), (0,2), (1,2)]], [3, 2, [(0,1), (1,1), (2,0), (2,1)]]]]
-p9 = [9, 4, (3,2), 4, [[3, 2, [(0,1), (0,2), (1,0), (1,1)]], [2, 3, [(0,0), (1,0), (1,1), (2,1)]], [3, 2, [(0,0), (0,1), (1,1), (1,2)]], [2, 3, [(0,1), (1,0), (1,1), (2,1)]]]]
+#information of each piece
+#[id, num of spaces each piece cover, (num of possible grid location in a row, num of possible grid location in a column), possible position, (grid_h, grid_w, grid location)]
+p1 = [1, 1, (7,7), 1, [[1, 1, [(0,0)]]]]
+p2 = [2, 2, (7,6), 2, [[1, 2, [(0,0), (0,1)]], [2, 1, [(0,0), (1,0)]]]]
+p3 = [3, 3, (6,6), 4, [[2, 2, [(0,0), (0,1), (1,1)]], [2, 2, [(0,1), (1,1), (1,0)]], [2, 2, [(0.0), (1,0), (1,1)]], [2, 2, [(1,0), (0,0), (0,1)]]]]
+p4 = [4, 3, (7,5), 2, [[1, 3, [(0,0), (1,0), (2,0)]], [3, 1, [(0,0), (0,1), (0,2)]]]]
+p5 = [5, 4, (6,6), 1, [[2, 2, [(0,0), (0,1), (1,1), (1,0)]]]]
+p6 = [6, 4, (6,5), 4, [[2, 3, [(0,1), (1,0), (1,1), (1,2)]], [3, 2, [(0,0), (1,0), (1,1), (2,0)]], [2, 3, [(0,0), (0,1), (0,2), (1,1)]], [3, 2, [(0,1), (1,0), (1,1), (0,2)]]]
+p7 = [7, 4, (7,4), 2, [[1, 4, [(0,0), (0,1), (0,2), (0,3)]], [4, 1, [(0,0), (1,0), (2,0), (3,0)]]]]
+p8 = [8, 4, (6,5), 8, [[2, 3, [(0,2), (1,0), (1,1), (1,2)]], [3, 2, [(0,0), (1,0), (2,0), (2,1)]], [2, 3, [(0,0), (0,1), (0,2), (1,0)]], [3, 2, [(0,0), (0,1), (1,1), (2,1)]], [3, 2, [(0,0), (1,0), (1,1), (1,2)]], [2, 3, [(0,0), (0,1), (1,0), (2,0)]], [3, 2, [(0,0), (0,1), (0,2), (1,2)]], [2, 3, [(0,1), (1,1), (2,0), (2,1)]]]]
+p9 = [9, 4, (6,5), 4, [[2, 3, [(0,1), (0,2), (1,0), (1,1)]], [3, 2, [(0,0), (1,0), (1,1), (2,1)]], [2, 3, [(0,0), (0,1), (1,1), (1,2)]], [3, 2, [(0,1), (1,0), (1,1), (2,1)]]]]
 
 piece_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9]
 
+piece_point = [1, 2, 3, 3, 4, 4, 4, 4, 4]
+
+#accumulated possible action for each piece
 action_marking = [0, 64, 176, 372, 468, 517, 685, 765, 1101, 1269]
 
 
@@ -47,7 +52,6 @@ class BlokusEnv(gym.Env):
     @property
     def observation(self):
         #combine current board state, legal_actions
-        #return combined observation
         copied_board = copy.deepcopy(self.board)
         combined_observation = np.array(copied_board).flatten()
         combined_observation.append(self.legal_actions())
@@ -56,17 +60,44 @@ class BlokusEnv(gym.Env):
 
     @property
     def legal_actions(self, action):
-        #calculate legal_actions based on board state, player's pieces
         legal_actions = np.zeros(self.possible_action_num)
-        for piece_num in self.current_player.pieces:
-            if piece:
+        for piece in [self.pieces[x] for x in range(self.piece_num) if self.current_player.pieces[x]==1]:
             for point in get_point(self.current_player.possible_point):
-                for loc in self.current_player.loc:
-                    if 
-                    action_num=
-                    legal_actions[action_num] = 1
-                    
-                    
+                for pos in piece.pos_list:
+                    for i in range(pos.h-1):
+                        for j in range(pos.w-1):
+                            r = point[0]+i
+                            c = point[1]+j
+                            if chk_possible(r,c,pos.loc) and chk_occupied(r,c,pos.loc) and chk_adjacent(r,c,pos.loc):
+                                action_num = action_marking[piece.id] + (pos.id-1)*piece.dim_size + (loc_w-(j-1))*(self.board_length-(w-1)) + loc_h-(i-1)
+                                legal_actions[action_num] = 1
+
+
+    def chk_possible(self, r, c, pos):
+        for grid in pos.loc:
+            grid = [grid[0]+r-1, grid[1]+c-1]
+            for point in get_point(self.current_player.possible):
+                if grid==point:
+                    return True
+        return False
+
+
+    def chk_occupied(self, r, c, pos):
+        for grid in pos.loc:
+            grid = [grid[0]+r-1, grid[1]+c-1]
+            for point in get_point(self.board)
+                if grid==point:
+                    return False
+        return True
+
+
+    def chk_adjacent(self, r, c, pos):
+        for grid in pos.loc:
+            grid = [grid[0]+r-1, grid[1]+c-1]
+            for point in get_point(self.current_player.adjacents):
+                if grid==point:
+                    return False
+        return True
 
 
     @property
@@ -76,11 +107,11 @@ class BlokusEnv(gym.Env):
 
     def get_point(self, board):
         point_list = []
-        for col in board:
-            for row in col:
-                if 
-                point = Point(col, row)
-                point_list.append(point)
+        for i in range(self.board):
+            for j in range(self.board):
+                if board[i][j]==1:
+                    point = (i, j)
+                    point_list.append(point)
         return point_list
 
 
@@ -89,6 +120,7 @@ class BlokusEnv(gym.Env):
         self.players = [Player(1), Player(2)]
         for player in self.players:
             player.setup(self.board_length, self.piece_num)
+        self.left_players = [1, 1]
         self.pieces = []
         for piece in piece_list:
             self.pieces.append(Piece(*piece))
@@ -104,23 +136,57 @@ class BlokusEnv(gym.Env):
         update_board(translate_action(action))
         self.current_player.update(translate_action(action))
 
-        self.change_turn()
+        done = self.change_turn()
 
-        while !get_point(self.current_player.possible_point):
-            
-
-            if no player to play:
-                done = True
+        while not get_point(self.current_player.possible_point):
+            self.left_players[self.current_player_num] = 0
+            done = self.change_turn()
+            if done=True:
+                break
 
         observation = self.observation()
+
+        if done:
+            reward = self.choose_winner()
+
         return obseration, reward, done, {}
-        
+
+
     def update_board(self, action):
+        _, loc_h, loc_w, pos = *action
+        h, w, loc = *pos
+        for grid in loc:
+            self.board[grid[0]+loc_h-(h-1)][grid[1]+loc_w-(w-1)] = self.current_player.id
+
+
+    def choose_winner(self):
+        int ans = 0
+        score=[]
+        for player in self.players:
+            for i in range(self.piece_num):
+                if player.pieces[i]==1:
+                    ans+=piece_point[i]
+            score.append(score)
+            ans=0
+        if score[0]>score[1]:
+            score = [1,-1]
+        else if score[0]<score[1]:
+            score = [-1,1] 
+        else:
+            score = [0,0]
 
 
     def change_turn(self):
+        done = True
+        for i in range(self.n_player):
+            if self.left_players[i] == 1:
+                done = False
+                break
+            
         self.current_player_num = (self.current_player_num+1)%self.n_player
-
+        while self.left_player[self.current_player_num]==0:
+            self.current_player_num = (self.current_player_num+1)%self.n_player
+        return done
 
     def translate_action(self, action):
         #translated_action int->id,loc_h,loc_w,pos
@@ -132,15 +198,15 @@ class BlokusEnv(gym.Env):
                 translated_action.append(id)
                 break
 
-        pos_num = action % self.pieces[id].dim
+        pos_num = (action-1) // self.pieces[id].dim + 1
         pos = self.pieces[id].pos_list[pos_num]
-        loc_h = self.pieces[id]
-        loc_w = self.pieces[id]
-        translated_action.append(h)        
-        translated_action.append(w)
+        loc_num = (action-1) % self.pieces[id].dim + 1
+        loc_h = (loc_num-1) // self.pieces[id].dim[1] + (pos.h-1)
+        loc_w = (loc_num-1) % self.pieces[id].dim[1] + (pos.w-1)
+        translated_action.append(loc_h)        
+        translated_action.append(loc_w)
         translated_action.append(pos)
         return translated_action
-
 
 
     def render(self, mode='human', close=False, verbose = True):
--- app/environments/blokus/blokus/envs/classes.py
+++ app/environments/blokus/blokus/envs/classes.py
@@ -1,22 +1,24 @@
+import numpy as np
+
 class Piece():
     def __init__(self, id, n, dim, pos, poss):
         self.id = id
         self.n = n
         self.dim = dim
+        self.dim_size = dim[0]*dim[1]
         self.pos = pos
         self.pos_list = []
+        int num=1
         for p in poss:
-            self.pos_list.append(Pos(*p))
-
-    def return_pos(self, num):
-        return pos_list[num]
+            self.pos_list.append(num, Pos(*p))
+            num+=1
 
 
 class Pos():
-    def __init__(self, w, h, loc):
+    def __init__(self, id, h, w, loc):
+        self.id = id
         self.h = h
         self.w = w
-        #self.loc = [[0 for i in range(h)] for j in range(w)]
         #tuple for point of piece as list
         self.loc = loc
 
@@ -27,54 +29,66 @@ class Player():
 
 
     def setup(self, board_length, piece_num):
-        self.pieces = [0 for i in range(piece_num)]
-        self.occupied = [[0 for i in range(board_length)] for j in range(board_length)]
-        self.corners = [[0 for i in range(board_length)] for j in range(board_length)]
-        self.adjacents = [[0 for i in range(board_length)] for j in range(board_length)]
-        self.possible = [[0 for i in range(board_length)] for j in range(board_length)]
+        self.pieces = np.ones(piece_num, dtype=int32)
+        self.occupied = np.zeros((board_length, board_length), dtype=int32)
+        self.corners = np.zeros((board_length, board_length), dtype=int32)
+        self.adjacents = np.zeros((board_length, board_length), dtype=int32)
+        self.possible = np.zeros((board_length, board_length), dtype=int32)
         
         self.possible[0][0] = self.possible[0][-1] = self.possible[-1][0] = self.possible[-1][-1] = 1
 
 
-    def update(self, action):
+    def update(self, action, board):
         self.occupied_update(action)
         self.corners_update(action)
         self.adjacents_update(action)
-        self.possible_update(action)
+        self.possible_update(action, board)
 
 
     def delete_piece(self, action):
-        id, _, _ , _ = *action
+        id, _, _, _ = *action
         self.pieces[id] = 0
 
 
     def occupied_update(self, action):
-        id, loc_w, loc_h, pos = *action
+        id, loc_h, loc_w, pos = *action
         occupied_update_loc = []
-        y,x = (h-1)+loc.h, (w-1)+loc.w
+        i,j = (h-1)+loc.h, (w-1)+loc.w
         for pos in pos.loc:
             grid = list(pos)
-            self.occupied[grid[0]+y, grid[1]+x] = 1
+            self.occupied[grid[0]+i, grid[1]+j] = 1
 
 
     def corners_update(self, action):
         id, loc_w, loc_h, pos = *action
         occupied_update_loc = []
-        y,x = (h-1)+loc.h, (w-1)+loc.w
+        i,j = (h-1)+loc.h, (w-1)+loc.w
+        dir = [(1,1), (1,-1), (-1,1), (-1,-1)]
         for pos in pos.loc:
-            grid = list(pos)
-            self.occupied[grid[0]+y+1, grid[1]+x+1] = self.occupied[grid[0]+y+1, grid[1]+x-1] = self.occupied[grid[0]+y-1, grid[1]+x+1] = self.occupied[grid[0]+y-1, grid[1]+x-1] = 1
+            grid = [grid[0]+i, grid[1]+j]
+            for d in dir:
+                x = grid[0]+dir[0]
+                y =  grid[1]+dir[1]
+                if x>=0 and x<8 and y>=0 and y<8:
+                    self.corners[x, y] = 1
+            
 
 
     def adjacents_update(self, action):
         id, loc_w, loc_h, pos = *action
         occupied_update_loc = []
-        y,x = (h-1)+loc.h, (w-1)+loc.w
+        i,j = (h-1)+loc.h, (w-1)+loc.w
+        dir = [(1,0), (0,1), (-1,0), (0,-1)]
         for pos in pos.loc:
-            grid = list(pos)
-            self.occupied[grid[0]+y+1, grid[1]+x] = self.occupied[grid[0]+y, grid[1]+x+1] = self.occupied[grid[0]+y-1, grid[1]+x] = self.occupied[grid[0]+y, grid[1]+x-1] = 1
+            grid = [grid[0]+i, grid[1]+j]
+            for d in dir:
+                x = grid[0]+dir[0]
+                y =  grid[1]+dir[1]
+                if x>=0 and x<8 and y>=0 and y<8:
+                    self.corners[x, y] = 1
 
 
-    def possible_update(self, action):
-        for i in 
+    def possible_update(self, action, board):
+        self.possible = self.corners - self.occupied - self.adjacents - board
+        np.clip(self.possible, 0, 1, out=self.possible)
 
